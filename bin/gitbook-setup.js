77cd0209521df438a668966eb53efb811d54574c
Rudolf Cicko <rudolfcicko2016@gmail.com>
2017-02-13 11:44:28 +0000

    Command -n [book name] allows to create a basic book structure

--- bin/gitbook-setup.js
+++ bin/gitbook-setup.js
@@ -5,15 +5,14 @@ var Task = require('shell-task')
 //var cli = require('../src');
 
 if (argv.n) {
-  console.log("Opcion -n ha sido utilizada con valor: " + argv.n || "no hay valor");
-  console.log("Alli vamos");
+  var bookName = argv.n;
+  new Task('gitbook init ' + bookName);
   //cli.deploy.deployTemplate(argv.n);
 } else if (argv.g) {
   console.log("Opción -g ha sido utilizada");
   //cli.githubRepo.createRepo(argv.u);
 } else {
-
     console.log("Comandos válidos:");
     console.log("gitbook-setup -n [NOMBRE LIBRO] --> Crea estructura del libro con nombre [NOMBRE LIBRO]");
-    console.log("gitbook-start -g -> Despliega libro en gitbook");
+    console.log("gitbook-setup -g -> Despliega libro en gitbook");
 }
