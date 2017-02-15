c5f15dc7de15f5e58f88031ebd5008a73147a9fa
Rudolf Cicko <rudolfcicko2016@gmail.com>
2017-02-15 23:06:07 +0000

    Proceeding on templates

--- bin/gitbook-setup.js
+++ bin/gitbook-setup.js
@@ -2,7 +2,6 @@
 var argv = require('minimist')(process.argv.slice(2));
 var Task = require('shell-task');
 var fs = require('fs');
-//var cli = require('../src');
 var templatesPath = require("path").join(__dirname, "../", "templates/");
 
 
@@ -10,25 +9,23 @@ var Tacks = require('tacks')
 var Dir = Tacks.Dir
 var File = Tacks.File
 
-var apiTemplate;
+var templateFiles = {};
 
-
-
-apiTemplate = new Tacks(Dir({
+/*
+template = new Tacks(Dir({
   'book.json': File(templatesPath + 'api/book.json'),
   'methods.md': File(templatesPath + 'api/methods.md')
 }));
+*/
 
-saveTemplates(templatesPath);
-
-
-
-function saveTemplates (basePath) {
+function saveTemplates (basePath, type) {
   console.log(basePath + " is the basePath");
   fs.readdirSync(basePath).forEach(function(file) {
-    console.log("the file to check is: " + file)
     if (fs.lstatSync(basePath + file).isDirectory()) {
-      saveTemplates(basePath + "" + file + '/');
+      if (type == file) {
+        console.log("the wanted type is: " + type);
+        saveTemplates(basePath + "" + file + '/');
+      }
     }
     else if (fs.lstatSync(basePath + file).isFile()){
       var filePath = basePath  + file;
@@ -36,35 +33,23 @@ function saveTemplates (basePath) {
         if (err) {
           return console.log(err);
         }
-        console.log("Data of " + filePath);
-        console.log(data);
+        templateFiles[file] = File(data);
       });
     }
   });
 }
 
+function createFilesForBook () {
+  template = new Tacks(Dir(templateFiles));
+  template.create(bookName);
+}
 
 if (argv.n) {
   var bookName = argv.n;
-  new Task('gitbook init ' + bookName).run(function (err, next) {
-    if (err) {
-      console.log("Error happen " + err);
-    }
-    else {
-      console.log("Book created!!");
-    }
-  });
-
-
-  //cli.deploy.deployTemplate(argv.n);
 } else if (argv.g) {
   console.log("Opción -g ha sido utilizada");
-
-  console.log(templatesPath);
-
-  //apiTemplate.create('templates');
-
-  //cli.githubRepo.createRepo(argv.u);
+  saveTemplates(templatesPath, argv.g);
+  createFilesForBook();
 } else {
     console.log("Comandos válidos:");
     console.log("gitbook-setup -n [NOMBRE LIBRO] --> Crea estructura del libro con nombre [NOMBRE LIBRO]");
