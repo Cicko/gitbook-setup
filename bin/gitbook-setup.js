67216ce45ccefad3114d72c5b6fa93ee9787b410
Rudolf Cicko <rudolfcicko2016@gmail.com>
2017-02-15 21:39:19 +0000

    Still working on files field

--- bin/gitbook-setup.js
+++ bin/gitbook-setup.js
@@ -2,8 +2,29 @@
 
 var argv = require('minimist')(process.argv.slice(2));
 var Task = require('shell-task');
+var fs = require('fs');
 //var cli = require('../src');
-var templatesPath = require("path").join(__dirname, "templates");
+var templatesPath = require("path").join(__dirname, "../", "templates/");
+
+
+saveTemplates(templatesPath);
+
+
+function saveTemplates (basePath) {
+  console.log(basePath + " is the basePath")
+  fs.readdirSync(basePath).forEach(function(file) {
+    console.log("the file to check is: " + file)
+    if (fs.lstatSync(basePath + file).isDirectory()) {
+      console.log(file + " is a directory");
+      saveTemplates(basePath + "" + file + '/');
+    }
+    else if (fs.lstatSync(basePath + file).isFile()){
+      console.log(file + " is a file");
+      require(basePath + file);
+    }
+  });
+}
+
 
 if (argv.n) {
   var bookName = argv.n;
