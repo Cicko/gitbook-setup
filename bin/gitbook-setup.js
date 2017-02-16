33b56f40b47c63945e8a4b2045ad0095d71725ed
Rudolf Cicko <rudolfcicko2016@gmail.com>
2017-02-16 17:20:12 +0000

    Fixing recursive folders on template

--- bin/gitbook-setup.js
+++ bin/gitbook-setup.js
@@ -3,7 +3,7 @@
 (function () {
   var argv = require('minimist')(process.argv.slice(2));
   var Task = require('shell-task');
-  var fs = require('fs-promise');
+  var fs = require('fs');
   var path = require('path');
   var promise = require('promise');
   var templatesPath = path.join(__dirname, "../", "templates/");
@@ -12,17 +12,26 @@
   var Dir = Tacks.Dir
   var File = Tacks.File
 
-  var templateFiles = {};
+  var wantedTemplate = {};
   var bookName = argv.n || "NoNameBook";
   var type = argv.t || "book";
   var help = argv.h != null;
 
-  function loadTemplates (basePath) {
+  /**
+  * This method load the files into the wantedTemplate object
+  * - basePath: is the path where it starts to search the wanted folder specified by the variable type
+  * - templatesFolderRoot: a boolean parameter that indicates if is looking for the root folder of the wanted type.
+  * That means that the first time the method is called the templatesFolderRoot parameter will be true because it
+  * will be looking in the templates folder. After that will be false because it will be looking in the
+  * subdirectories.
+  **/
+  function loadTemplates (basePath, templatesFolderRoot, parentFolder) {
+    var filesInFolder = {};
     fs.readdirSync(basePath).forEach(function(file) {
       if (fs.lstatSync(basePath + file).isDirectory()) {
-        if (type == file) {
-          loadTemplates(basePath + "" + file + '/');
-        }
+        var subDirectoryPath = basePath + "" + file + '/';
+        if (type == file) loadTemplates(subDirectoryPath, false, null);
+        else if (!templatesFolderRoot) loadTemplates(subDirectoryPath, false, file);
       }
       else if (fs.lstatSync(basePath + file).isFile()){
         var filePath = basePath + file;
@@ -30,14 +39,18 @@
           if (err) {
             return console.log(err);
           }
-          templateFiles[file] = File(data);
+          if (!parentFolder)
+            wantedTemplate[file] = File(data);
+          filesInFolder[file] = File(data);
         });
       }
-    })
+    });
+    if (parentFolder)
+      wantedTemplate[parentFolder] = Dir(filesInFolder);
   }
 
   function exportTemplate () {
-    template = new Tacks(Dir(templateFiles));
+    template = new Tacks(Dir(wantedTemplate));
     var exportPath = path.join(process.cwd(), "/" , bookName);
     template.create(exportPath);
   }
@@ -48,7 +61,7 @@
       console.log("gitbook-setup -g -> Despliega libro en gitbook");
   }
   else {
-      loadTemplates(templatesPath);
+      loadTemplates(templatesPath, true, null);
       setTimeout(function() {
         exportTemplate();
       }, 1000);
