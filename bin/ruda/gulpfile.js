79e487a67d5411582f2aeea261b0b02c8d925691
Rudolf Cicko <rudolfcicko2016@gmail.com>
2017-04-08 22:25:32 +0100

    Removed ruda folder

--- bin/ruda/gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var gghPages = require('gulp-gh-pages');
-var ghPages = require('gh-pages')
-var exec = require('child_process').exec;
-
-
-gulp.task('deploy-gh-pages', [], function() {return gulp.src('./_book/**/*').pipe(gghPages());});
