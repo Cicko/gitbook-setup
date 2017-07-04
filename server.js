c3492dc6e0dd503b6bfdd746baf9b6d0fe84e882
Cicko <rudolfcicko2016@gmail.com>
2017-07-04 03:00:16 +0100

    Added more content to index.ejs

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var port = 3000;
 app.use('/', express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
+
 app.listen(port, function() {
     console.log('Your files will be served through this web server in port ' + port);
 });
