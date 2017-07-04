1f72216a976afda6560bed29ea3de92c93655df4
Cicko <rudolfcicko2016@gmail.com>
2017-07-04 02:51:02 +0100

    Added some files to open app in browser

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var path = require('path')
 var app = express();
-app.use('/', express.static(path.join(__dirname, 'views')));
 var port = 3000;
 
+
+
+app.use('/', express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','ejs');
 app.listen(port, function() {
     console.log('Your files will be served through this web server in port ' + port);
 });
