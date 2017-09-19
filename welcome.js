f116a8c79296002d40554863506cef780253b2c7
Cicko <rudolfcicko2016@gmail.com>
2017-09-19 16:26:32 +0100

    Changing things

--- welcome.js
+++ welcome.js
@@ -1,11 +1,2 @@
-#!/usr/bin/env node
-var opn = require('opn')
-
-opn("https://cicko.github.io/gitbook-setup/")
-   .then ((val) => {
-	console.log("Showing details about usability " + val)
-  })
-   .catch ((err) => {
-       console.log("ERROR " + err)
-   })
+require("openurl").open("https://cicko.github.io/gitbook-setup/")
 
