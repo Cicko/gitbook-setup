aebd1fe3e2ede5401c6dc7d6ed34c6e9cbdd5039
Cicko <rudolfcicko2016@gmail.com>
2017-09-19 16:19:51 +0100

    Adding promise handling

--- welcome.js
+++ welcome.js
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
 var opn = require('opn')
 
-opn("https://cicko.github.io/gitbook-setup/");
+opn("https://cicko.github.io/gitbook-setup/")
+   .then ((val) => {
+	console.log("Showing details about usability " + val)
+  }).
+   .catch ((err) => {
+       console.log("ERROR " + err)
+   })
+
